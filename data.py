8f8c5e5810625b7f20575aec9de1792414361ade
João Pablo <jp.pablolima@gmail.com>
2024-09-17 07:14:25 -0300

    Sistema Bancário update

--- data.py
+++ data.py
@@ -1,4 +1,42 @@
-from  datetime import  datetime
+# def analise_vendas(vendas):
+#   total_vendas = sum(vendas)
+#   media_vendas = total_vendas / len(vendas)
+#   return f"{total_vendas}, {media_vendas:.2f}"
+
+# def obter_entrada_vendas():
+#     # Solicita a entrada do usuário em uma única linha
+#     entrada = input()
+#     vendas  = list(map(int, entrada.split(",")))
+#     return vendas
+
+# vendas = obter_entrada_vendas()
+# print(analise_vendas(vendas))
 
 
-print(datetime.today())
+def produto_mais_vendido(produtos):
+    contagem = {}
+    
+    for produto in produtos:
+        if produto in contagem:
+            contagem[produto] += 1
+        else:
+            contagem[produto] = 1
+    
+    max_produto = None
+    max_count = 0
+    
+    for produto, count in contagem.items():
+        if count > max_count:
+            max_count = count
+            max_produto = produto
+          
+    return max_produto
+
+def obter_entrada_produtos():
+    
+    entrada = input()
+    produtos = [produto.strip() for produto in entrada.split(",")]
+    return produtos
+
+produtos = obter_entrada_produtos()
+print(produto_mais_vendido(produtos))
